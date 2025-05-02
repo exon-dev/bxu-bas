103cd01e7fb1cac3de5a7aaed47ebfacad6557b6
ImTrikk <patrickdionenofficial@gmail.com>
2025-05-02 21:59:13 +0800

    update: 🔨 update API and APP URLs to use production environment

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
- API_BASE_URL: "http://127.0.0.1:8000/api",
- APP_URL: "http://127.0.0.1:8000",
- // API_BASE_URL: "https://bxubas-backend-app.onrender.com/api",
- // APP_URL: "https://bxubas-backend-app.onrender.com",
+ // API_BASE_URL: "http://127.0.0.1:8000/api",
+ // APP_URL: "http://127.0.0.1:8000",
+ API_BASE_URL: "https://bxubas-backend-app.onrender.com/api",
+ APP_URL: "https://bxubas-backend-app.onrender.com",
 };
 export default CONFIG;
